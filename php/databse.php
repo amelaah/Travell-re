efde2cc9e6f046dad3889ba3d6ed2f28fd6f713b
amelaah <ah70135@ubt-uni.net>
2025-01-29 12:39:16 +0100

    idk
    😭 😭 😭 🆘

---
+++ php/databse.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password ="";
+$databse ="travelly";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if($conn->connect_error){
+    die("Connention failed!:". $conn->connect_error);
+}
+
+?>
+
