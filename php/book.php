c27b3c0ebfb9c85599ca64e9cd3bafdbd9eac252
amelaah <ah70135@ubt-uni.net>
2025-01-31 23:28:30 +0100

    .

---
+++ php/book.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+header("Cache-Control: no-cache, no-store, must-revalidate"); 
+header("Pragma: no-cache"); 
+header("Expires: 0");
+
+include 'database.php';
+
+
+if ($_SERVER["REQUEST_METHOD"]  == "POST") {
+    $first_name =$_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email= $_POST['email'];
+    $password= password_hash($_POST['password'],PASSWORD_DEFAULT); 
+
+    $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $first_name, $last_name, $email, $password);
+
+    if ($stmt->execute()) {
+        echo "Registration successful!";
+
+        $_SESSION['user_id'] = $conn->insert_id; 
+        $_SESSION['first_name'] = $first_name;
+
+        
+        setcookie('is_logged_in', 'true', time() + (86400 * 30), '/');
+
+        if (isset($_POST['remember_me'])) {
+           
+            setcookie("user_email", $email, time() + (30 * 24 * 60 * 60), "/", "", true, true);
+          
+        } 
+            header("Location: travelly.html");
+            exit();
+        
+    }    else  {
+        
+        echo "Error:" . $conn->error;
+    }
+
+
+ $stmt->close();
+ $conn->close();
+}
+?>
