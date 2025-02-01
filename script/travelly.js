560ae9facb67682703d1736c7d6e3a9eaf709698
viona <vk70133@ubt-uni.net>
2025-02-01 03:58:47 +0100

    😵

--- script/travelly.js
+++ script/travelly.js
@@ -11,10 +11,23 @@ window.addEventListener("scroll", function () {
 const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown_menu");
+const menuItems = document.querySelectorAll(".dropdown_menu li");
+
+window.onload = () => {
+  dropDownMenu.classList.remove("open"); //e fsheh menun drop down kur behet faqja refresh
+  // dropDownMenu.style.display = "none"
+};
 
 toggleBtn.onclick = function () {
   dropDownMenu.classList.toggle("open");
   const isOpen = dropDownMenu.classList.contains("open");
 
-  toggleBtnIcon.classList = isOpen;
+  menuItems.forEach((item) => {
+    //kur nje 'li' (menu item) klikohet ne menu atehere ajo menu dropdown mbyllet
+    item.addEventListener("click", () => {
+      dropDownMenu.classList.remove("open");
+      toggleBtnIcon.classList.replace("fa-xmark", "fa-bars");
+    });
+  });
+  //  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
