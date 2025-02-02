e8903895b129698501ec6188a4e2164825871811
viona <vk70133@ubt-uni.net>
2025-02-02 23:08:05 +0100

    Update food.js

--- script/food.js
+++ script/food.js
@@ -3,13 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   dropdowns.forEach((dropdown) => {
     dropdown.addEventListener("click", function (event) {
-      // Prevent default dropdown behavior and toggle display
       event.stopPropagation();
       this.classList.toggle("hidden");
     });
   });
 
-  // Hide the dropdown when clicking outside
   document.addEventListener("click", function () {
     dropdowns.forEach((dropdown) => {
       dropdown.classList.add("hidden");
