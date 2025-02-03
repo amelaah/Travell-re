9de984a05ed0f20d052c9727c6d984b122bb0633
amelaah <ah70135@ubt-uni.net>
2025-02-04 00:19:42 +0100

    ups pr3

--- php/database.php
+++ php/database.php
@@ -9,18 +9,18 @@ $database ="travelly";
 $conn =mysqli_connect($servername, $username, $password, $database);
 
 // nese lidhja sosht succecful:(
-if(!$conn->connect_error){
+if($conn->connect_error){
     die("Connection failed!:". $conn->error);
 }
 
-// funksioni per user 
+// funksioni per user
 function getAllUsers($conn){
     $sql = "SELECT * FROM users";
     $result = $conn->query($sql);
 
     $users = [];
     while ($row = $result->fetch_assoc()) {
-        $users[] = $rows;
+        $users[] = $row;
     }
     return $users;
 }
