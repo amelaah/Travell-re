f3cab159df665b862218471daa5cade10fd56caa
viona <vk70133@ubt-uni.net>
2025-02-03 13:13:22 +0100

    oop login

--- php/book.php
+++ php/book.php
@@ -6,7 +6,7 @@ include 'database.php';
 
 //kontrollon nese kerkesa eshte bere me metoden POST
 if ($_SERVER["REQUEST_METHOD"]  == "POST") {
-    //marrim te dhenat e formularit qe i ka dergu perdoruesi gjat regjistrimit
+    //i merr te dhenat e futura nga perdoruesi    
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
--- php/login.php
+++ php/login.php
@@ -1,55 +1,24 @@
 <?php
-session_start();
+require_once 'user.php';  //perfshin user.php per me perdor klasen user
 
-// i refresh faqja te dhanat mos me i cache
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: 0");
-
-//databaza
-include "database.php";
-
-//i merr te dhanat i shtin neper variabla
+//kontrollon nese formulari u dorzu me metoden POST 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+   //i merr te dhenat e futura nga perdoruesi
    $email = $_POST['email'];
    $password = $_POST['password'];
-
-   $stmt = $conn->prepare("SELECT id, first_name, password FROM users WHERE email = ?"); //statement i sql
-   $stmt->bind_param("s", $email);     //tregon qe email eshte string
-   $stmt->execute();  //e ekzekuton statement
-   $stmt->store_result();
+   $remember_me = isset($_POST['remember_me']);
 
 
-   //kontrollon a ka users me ate email
-   if ($stmt->num_rows > 0) {
-      $stmt->bind_result($user_id, $first_name, $hashed_password);  //i shtin neper variabla te dhenat e marra nga user ne databaze
-      $stmt->fetch();
+   //krijon nje User objekt
+   $user = new User($conn);
 
-      //kontrollon  nese password i user eshte i njejt me hashed password
-      if (password_verify($password, $hashed_password)) {
-         $_SESSION['user_id'] = $user_id;  //i shtin te dhanat e user ne sesione per ti mbajt ne mend
-         $_SESSION['first_name'] = $first_name;
-
-          setcookie('is_logged_in', 'true', time() + (86400 * 30), '/');  //cookie per 30 dite
-
-          //kontrollon a eshte check 'remember me'
-         if (!empty($_POST['remember_me'])) {
-            //nese po set cookies per 30 dite
-            setcookie("user_email", $email, time() + (86400 * 30), "/");
-            setcookie("user_password", $password, time() + (86400 * 30), "/");
-         }
-            header("Location: /html/travelly.html");  //te qon ne travelly.html
-            exit();
-         
-      } else {
-            echo "Incorrect password!";
-         }
-      } else {
-         echo "No user found with that e-mail!";
-      }
-      
-//e mbyll sql statement edhe lidhjen me databazen
-   $stmt->close();
-   $conn->close();
+   //provon me identifiku userin
+   if ($user->login($email, $password, $remember_me)) {
+      header("Location: /html/travelly.html"); //nese hyrja eshte e suksesshme e dergon ne faqen kryesore
+      exit();
+   } else {
+      echo "Incorrect email or password!"; //nese deshton, dergon ket mesazh
+   }
 }
 ?>
+   
