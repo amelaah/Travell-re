6af4e896c8bd6f0a135baa79f1c940d1dae1b408
viona <vk70133@ubt-uni.net>
2025-02-04 14:29:26 +0100

    Update food.js

--- script/food.js
+++ script/food.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dropdowns = document.querySelectorAll("select");
+  const dropdowns = document.querySelectorAll("select"); //i ben select tgjitha elementet dropdown
 
   dropdowns.forEach((dropdown) => {
+    //kur klikon mbi select e hap dropdownin te parazgjedhur te shfletuesit
     dropdown.addEventListener("click", function (event) {
-      event.stopPropagation();
-      this.classList.toggle("hidden");
+      event.stopPropagation(); //parandalon perhapjen e ngjarjes ne page
+      this.classList.toggle("hidden"); //
     });
   });
 
+  //kur klikon tjeter kun ne page i fsheh dropdowns
   document.addEventListener("click", function () {
     dropdowns.forEach((dropdown) => {
       dropdown.classList.add("hidden");
